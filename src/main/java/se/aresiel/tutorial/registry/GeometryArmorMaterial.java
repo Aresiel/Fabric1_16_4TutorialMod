0e68cc0907eeb39f30eda018b7817b6ae433ac5b
Aresiel <love.dittlau@gmail.com>
2021-02-25 18:50:55 +0100

    Added the Square armorset with recipes and textures, productive commit!

---
+++ src/main/java/se/aresiel/tutorial/registry/GeometryArmorMaterial.java
@@ -0,0 +1,66 @@
+package se.aresiel.tutorial.registry;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class GeometryArmorMaterial implements ArmorMaterial {
+
+    private static int BASE_DURABILITY;
+    private static int PROTECTION_VALUE;
+    private static int enchantability;
+    private static float knockbackResistance;
+    private static int  toughness;
+
+    public GeometryArmorMaterial(int level) {
+        BASE_DURABILITY = (int) Math.pow(4, level)*10;
+        PROTECTION_VALUE = (int) Math.pow(4, level);
+        enchantability = (int) Math.pow(4, level);
+        knockbackResistance = (float) Math.pow(2, level);
+        toughness = (int) Math.pow(4, level);
+    }
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION_VALUE;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_IRON;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Registry.ITEM.get(Identifier.tryParse("minecraft:nether_star")));
+    }
+
+    @Override
+    public String getName() {
+        return "square";
+    }
+
+    @Override
+    public float getToughness() {
+        return toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return knockbackResistance/10;
+    }
+}
--- src/main/java/se/aresiel/tutorial/registry/ModItems.java
+++ src/main/java/se/aresiel/tutorial/registry/ModItems.java
@@ -1,20 +1,34 @@
 package se.aresiel.tutorial.registry;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.SwordItem;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import se.aresiel.tutorial.Tutorial;
 
 public class ModItems {
-    public static final GeometryToolItem POINT = new GeometryToolItem(0);
-    public static final GeometryToolItem LINE = new GeometryToolItem(1);
-    public static final GeometryToolItem SQUARE = new GeometryToolItem(2);
+
+    //Items / Weapons
+    public static final Item POINT = new GeometryToolItem(0);
+    public static final Item LINE = new GeometryToolItem(1);
+    public static final Item SQUARE = new GeometryToolItem(2);
+
+    //Armor
+    public static final ArmorMaterial squareArmorMaterial = new GeometryArmorMaterial(2);
+
+    public static final Item SQUARE_HELMET = new ArmorItem(squareArmorMaterial, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item SQUARE_CHESTPLATE = new ArmorItem(squareArmorMaterial, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item SQUARE_LEGGINGS = new ArmorItem(squareArmorMaterial, EquipmentSlot.LEGS, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item SQUARE_BOOTS = new ArmorItem(squareArmorMaterial, EquipmentSlot.FEET, new Item.Settings().group(ItemGroup.COMBAT));
 
     public static void registerItems(){
         Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "point"), POINT);
         Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "line"), LINE);
         Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "square"), SQUARE);
+
+        Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "square_helmet"), SQUARE_HELMET);
+        Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "square_chestplate"), SQUARE_CHESTPLATE);
+        Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "square_leggings"), SQUARE_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier(Tutorial.MOD_ID, "square_boots"), SQUARE_BOOTS);
     }
 }
